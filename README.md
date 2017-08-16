6dbb7b45a991f740cd95e434a6456b32256ca9ff
Jelle Dekker <jelle.dekker@bizzomate.com>
2017-08-16 10:51:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# LeaveAction
+
+This widget allows you to configure a microflow to be executed when the user exits the page in a non intended manner. E.g. by using the navigation or by closing the tab/browser.
+ 
+##Features and limitations
+* Allows a microflow to be executed when the user exits the page
+* Possible to show a confirmation dialog when the user closes the browser tab
+* Possible to specify which items that result in navigation should be excluded (default: .mx-button)
+* Widget will always be triggered when the user closes the browser tab, or uses browser navigation (back/forward)
+
+
+##Dependencies
+* Build and tested using Mendix 7.5.0
+
+## Contributing
+
+Want to contribute to this widget, submit a feature request or report a bug? Please use the [GitHub repository]()!
---
+++ src/LeaveAction/widget/LeaveAction.js
@@ -0,0 +1,142 @@
+/*global logger*/
+/*
+    LeaveAction
+    ========================
+
+    @file      : LeaveAction.js
+    @version   : 1.0.0
+    @author    : Jelle Dekker
+    @date      : 8/15/2017
+    @copyright : Bizzomate 2017
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+  "dojo/_base/declare",
+  "mxui/widget/_WidgetBase",
+  "dojo/_base/lang",
+  "dojo/_base/array",
+  "dojo/query",
+  "dojo/on"
+], function (declare, _WidgetBase, dojoLang, dojoArray, dojoQuery, dojoOn) {
+  "use strict";
+
+  // Declare widget's prototype.
+  return declare("LeaveAction.widget.LeaveAction", [_WidgetBase], {
+    // Parameters configured in the Modeler.
+    mfToExecute: "",
+    warningOnPageClose: "",
+    excludeSelector: "",
+
+    // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+    _contextObj: null,
+    _handles: null,
+    _active: false,
+
+    // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+    constructor: function () {
+      // Uncomment the following line to enable debug messages
+      logger.level(logger.DEBUG);
+      logger.debug(this.id + ".constructor");
+      this._handles = [];
+    },
+
+    // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+    postCreate: function () {
+      logger.debug(this.id + ".postCreate");
+
+      if (this.readOnly || this.get("disabled") || this.readonly) {
+        this._readOnly = true;
+      }
+
+      this._active = true;
+      this._handles.push(dojoOn(window, "beforeunload", dojoLang.hitch(this, this._onClose)));
+
+    },
+
+
+    // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+    update: function (obj, callback) {
+      logger.debug(this.id + ".update");
+
+      this._contextObj = obj;
+      this._setupEvents();
+      this._executeCallback(callback, ".update");
+    },
+
+    // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+    uninitialize: function () {
+      logger.debug(this.id + ".uninitialize");
+
+      if (this._active) {
+        this._execMf(this.mfToExecute, this._contextObj.getGuid());
+      }
+      dojoArray.forEach(this._handles, function (handle, i) {
+        handle.remove();
+      });
+      this._active = false;
+      // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+    },
+
+    // Attach events to HTML dom elements
+    _setupEvents: function () {
+      logger.debug(this.id + "._setupEvents");
+
+      this.connect(this.mxform, "onNavigation", function () {
+        if (this.excludeSelector && this.excludeSelector.trim().length > 0) {
+          dojoQuery(this.excludeSelector).forEach(dojoLang.hitch(this, function (node) {
+            this._handles.push(dojoOn(node, "click", dojoLang.hitch(this, this._deactivate)));
+          }));
+        }
+      });
+    },
+
+    _onClose: function (event) {
+      logger.debug(this.id + "._onClose");
+      this.uninitialize();
+      if (this.warningOnPageClose && this._active) {
+        event.preventDefault();
+      }
+    },
+
+    _deactivate: function (event) {
+      logger.debug(this.id + ".deactivate");
+      this._active = false;
+    },
+
+    _execMf: function (mf, guid, cb) {
+      logger.debug(this.id + "._execMf");
+      if (mf && guid) {
+        mx.ui.action(mf, {
+          params: {
+            applyto: "selection",
+            guids: [guid]
+          },
+          callback: dojoLang.hitch(this, function (objs) {
+            if (cb && typeof cb === "function") {
+              cb(objs);
+            }
+          }),
+          error: function (error) {
+            mx.ui.error("Error executing microflow " + mf + " : " + error.message);
+            console.error(this.id + "._execMf", error);
+          }
+        }, this);
+      }
+    },
+
+    _executeCallback: function (cb, from) {
+      logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+      if (cb && typeof cb === "function") {
+        cb();
+      }
+    }
+  });
+});
+
+require(["LeaveAction/widget/LeaveAction"]);
